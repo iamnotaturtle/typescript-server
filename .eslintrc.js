c7a18ec4dce25699368ac11156bac9bc8366ba47
Yakov <ygaberman@gmail.com>
2019-12-13 17:59:37 -0500

    Adds linting, typescript, README and debug launch

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# Project Name
+
+## Development
+```
+yarn        # install
+yarn dev    # develop
+yarn build  # build distribution 
+yarn start  # run dist folder
+```
